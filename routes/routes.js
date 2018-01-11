a5faae4e3fe0b8f222aecfe5c82e86f115eeb5a0
parkpoom <parkpoom.d@gmail.com>
2018-01-11 20:38:47 +0700

    create PUT /api/drivers/:id

--- routes/routes.js
+++ routes/routes.js
@@ -6,4 +6,5 @@ module.exports = (app) => {
   app.get('/api', DriversController.greeting);
 
   app.post('/api/drivers', DriversController.create);
+  app.put('/api/drivers/:id', DriversController.edit);
 };
