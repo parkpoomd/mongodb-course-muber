fc0f80ff855b685e719ac270fd3f779d27f85416
parkpoom <parkpoom.d@gmail.com>
2018-01-11 11:19:29 +0700

    first get api

--- app.js
+++ app.js
@@ -2,4 +2,10 @@ const express = require('express');
 
 const app = express();
 
+// Watch for incoming requests of method GET
+// to the route http://localhost:3000/api
+app.get('/api', (req, res) => {
+  res.send({ hi: 'there' });
+});
+
 module.exports = app;
